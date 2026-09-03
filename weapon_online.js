cb1e8f1fce750e7fa041021881fb3d3f3b12b2e6
immortality939 <49891884+immortality939@users.noreply.github.com>
2026-09-03 21:00:12 +0800

    Update weapon_online.js

--- weapon_online.js
+++ weapon_online.js
@@ -1,78 +1,123 @@
-// weapon_online.js - Works for both browser and server
+// weapon_online.js
+// SERVER SIDE WEAPON DATABASE
+// Used only by server.js for online multiplayer
 
-const WEAPONS_ONLINE = {
+
+const WEAPONS = {
+
   uzi: {
     name: "uzi",
-    damage: 4,
+
+    // Damage handled by server
+    damage: 400,
+
     fireRate: 10,
+
     reloadTime: 2000,
+
     bulletSpeed: 500,
+
     magazine: 25,
-    maxMagazine: 25
+    maxMagazine: 25,
+
+    fireSound: "uzi.ogg",
+
+    hitEffect: "9mm",
+
+    recoil: 0.05
   },
-  
+
+
   ak47: {
     name: "ak47",
+
     damage: 4,
+
     fireRate: 7,
+
     reloadTime: 2000,
+
     bulletSpeed: 500,
-    magazine: 350,
-    maxMagazine: 350
+
+    magazine: 35,
+    maxMagazine: 35,
+
+    fireSound: "ak47.ogg",
+
+    hitEffect: "9mm",
+
+    recoil: 0.09
   },
+
 
   pistol: {
     name: "pistol",
+
     damage: 8,
+
     fireRate: 4,
+
     reloadTime: 1500,
+
     bulletSpeed: 600,
+
     magazine: 12,
-    maxMagazine: 12
+    maxMagazine: 12,
+
+    fireSound: "pistol.ogg",
+
+    hitEffect: "9mm",
+
+    recoil: 2
   },
+
 
   shotgun: {
     name: "shotgun",
+
     damage: 6,
+
     fireRate: 2,
+
     reloadTime: 2500,
+
     bulletSpeed: 450,
+
     pellets: 5,
+
     spread: 0.25,
+
     magazine: 6,
-    maxMagazine: 6
+    maxMagazine: 6,
+
+    fireSound: "shotgun.ogg",
+
+    hitEffect: "shotgun",
+
+    recoil: 0.15
   }
+
 };
 
+
+// Get one weapon
 function getWeapon(name) {
-  const w = WEAPONS_ONLINE[name];
-  if (!w) return null;
-  return JSON.parse(JSON.stringify(w));
+
+  return WEAPONS[name] || null;
+
 }
 
-function getWeaponOnline(name) {
-  return getWeapon(name);
-}
 
+// Get all weapons
 function getAllWeapons() {
-  return Object.keys(WEAPONS_ONLINE);
+
+  return Object.values(WEAPONS);
+
 }
 
-function getAllWeaponsOnline() {
-  return getAllWeapons();
-}
 
-// For browser (global variables)
-if (typeof window !== "undefined") {
-  window.WEAPONS = WEAPONS_ONLINE;
-  window.WEAPONS_ONLINE = WEAPONS_ONLINE;
-  window.getWeapon = getWeapon;
-  window.getWeaponOnline = getWeaponOnline;
-  window.getAllWeapons = getAllWeapons;
-  window.getAllWeaponsOnline = getAllWeaponsOnline;
-}
-
-// For Node.js/server (CommonJS)
-if (typeof module !== "undefined" && module.exports) {
-  module.exports = { WEAPONS_ONLINE, getWeapon, getWeaponOnline, getAllWeapons, getAllWeaponsOnline };
-}
+export {
+  WEAPONS,
+  getWeapon,
+  getAllWeapons
+};
