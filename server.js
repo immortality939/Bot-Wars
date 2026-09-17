765411dcc936461816434d541e69718644ef90b3
immortality939 <49891884+immortality939@users.noreply.github.com>
2026-09-17 22:45:20 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -43,6 +43,16 @@ function cleanMapFileName(name) {
 }
 
 const httpServer = http.createServer((req, res) => {
+  // Allow the game APK / browser to request map downloads
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    res.writeHead(204);
+    res.end();
+    return;
+  }
 
   // -------------------------------------------------------------
   // CREATE MAP DOWNLOAD
